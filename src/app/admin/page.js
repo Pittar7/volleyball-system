097463acd9989db8465e5696700c869bd185eafb
Piotr <pspiewak07@gmail.com>
2026-02-21 22:27:17 +0100

    Added basic routing: home, admin, screen

---
+++ src/app/admin/page.js
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Panel Administratora ⚙️</h1>
+    </main>
+  );
+}
---
+++ src/app/screen/page.js
@@ -0,0 +1,7 @@
+export default function ScreenPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">TRYB TELEBIM 🖥</h1>
+    </main>
+  );
+}
